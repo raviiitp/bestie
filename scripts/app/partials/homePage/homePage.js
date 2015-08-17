028ed00a55d41e47a2c091e8284c9eb297a8ed9b
unknown <ravi.kumar@RKUMAR-HPD.FireEye.com>
2015-08-17 21:15:43 +0530

    some problem with ui-view

---
+++ scripts/app/partials/homePage/homePage.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('bestie')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('homePage', {
+                parent: 'site',
+                url: '/',
+                data: {
+                    roles: []
+                },
+                views: {
+                    'content@': {
+                        templateUrl: '/scripts/app/partials/homePage/homePage.html',
+                        //controller: 'HomePageController'
+                    }
+                },
+                resolve: {
+                }
+            });
+    });
